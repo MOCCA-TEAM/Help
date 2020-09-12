5e4d66ef0140c274473c6e84951f920a217cd133
JurandirGuilherme <JurandirGuilherme1@gmail.com>
2020-09-11 22:59:57 -0300

    Tradução de vairiáveis em foods

--- Backend/src/database/migrations/20200910195600_create_foods.js
+++ Backend/src/database/migrations/20200910195600_create_foods.js
@@ -2,9 +2,11 @@
 exports.up = function(knex) {
     return knex.schema.createTable('foods', function (table){
     table.increments();
-    table.string('tipo').notNullable();
-    table.decimal('preco').notNullable();
-    table.string('valornutricional').notNullable();
+    table.string('category').notNullable();
+    table.string('type').notNullable();
+    table.string('product').notNullable();
+    table.decimal('price').notNullable();
+    table.string('ntvalue').notNullable();
     table.string('petshop_id').notNullable();
     table.foreign('petshop_id').references('id').inTable('petshops');
     });

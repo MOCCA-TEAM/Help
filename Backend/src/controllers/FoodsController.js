e095e83b3fae0ffeef3a211d9a9a27aae49332b1
JurandirGuilherme <JurandirGuilherme1@gmail.com>
2020-09-15 20:28:36 -0300

    Criação do delete e comentários no arquivo index.js

--- Backend/src/controllers/FoodsController.js
+++ Backend/src/controllers/FoodsController.js
@@ -18,5 +18,20 @@ module.exports = {
             petshop_id,
         })
         return response.json({ id });
+    },
+    async delete(request, response){
+        const { id } = request.params;
+        const petshop_id = request.headers.authorization;
+
+        const foods = await connection('foods')
+        .where('id', id)
+        .select('petshop_id')
+        .first()
+        if (foods.petshop_id != petshop_id){
+            return response.status(401).json({ error: 'Operation not permitted'})
+        }
+        await connection('foods').where('id', id).delete()
+
+        return response.status(204).send();
     }
 };

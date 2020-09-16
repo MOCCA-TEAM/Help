dd4b32f15348055e920bd47a2f0aaec477aa9155
JurandirGuilherme <JurandirGuilherme1@gmail.com>
2020-09-16 19:44:27 -0300

    Novas rotas para cadastro de Item e Food

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,7 +4,9 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Logon from './pages/Logon';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
-import NewProduct from './pages/NewProduct';
+import NewProduct from './pages/NewProduct/index';
+import NewItem from './pages/NewProduct/NewItem/index';
+import NewFood from './pages/NewProduct/NewFood/index';
 
 export default function Routes(){
     return(
@@ -13,7 +15,9 @@ export default function Routes(){
                 <Route path="/" exact component={Logon} />
                 <Route path="/register" component={Register} />
                 <Route path="/profile" component={Profile} />
-                <Route path="/product/new" component={NewProduct}/>
+                <Route path="/product" component={NewProduct}/>
+                <Route path="/product/new/item" component={NewItem}/>
+                <Route path="/product/new/food" component={NewFood}/>
             </Switch>
         </BrowserRouter>
     );

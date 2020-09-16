3dce6a9e820ab5504c3b31e1fa15740134ed5e5a
JaoMendonca <victormendonca0@gmail.com>
2020-09-16 19:47:59 -0300

    Criando a parte geral dos petshops

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -5,6 +5,7 @@ import Logon from './pages/Logon';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
 import NewProduct from './pages/NewProduct';
+import Init from './pages/Init';
 
 export default function Routes(){
     return(
@@ -14,6 +15,7 @@ export default function Routes(){
                 <Route path="/register" component={Register} />
                 <Route path="/profile" component={Profile} />
                 <Route path="/product/new" component={NewProduct}/>
+                <Route path="/init" component={Init}/>
             </Switch>
         </BrowserRouter>
     );
